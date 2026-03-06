79787c7e1f088eb08ba23a69bfbca820c1271fcf
ATCuliuan <ayoyiculiuan@gmail.com>
2026-03-06 18:24:05 +0800

    naka connect na pero alam ko may error di ko lng ma try pano makikita yung error

---
+++ backend/src/supabaseAdmin.ts
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const supabaseUrl = 'https://hegosalockwketsqpqta.supabase.co'
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string
+
+export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey)
