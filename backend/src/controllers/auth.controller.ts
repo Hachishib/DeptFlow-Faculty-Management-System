37f1339b9972d849ad0f79bfe36c6b94ef848f3c
Dan-code25 <danjhenielbringas1125@gmail.com>
2026-03-07 22:26:30 +0800

    Initialized backend folders

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+
+export const googleLogin = (req: Request, res: Response) => {
+  res.end();
+};
---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { googleLogin } from "../controllers/auth.controller";
+
+const router = express.Router();
+
+router.post("/login", googleLogin);
+
+export default router;
